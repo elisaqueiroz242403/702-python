16729b659d9fbdc622f5ffbe942a51b0badac976
karen queiroz <kqueiroz812@gmail.com>
2019-08-13 17:59:58 -0300

    adicionar exercicio 2 e 3

---
+++ operacoes.py
@@ -0,0 +1,24 @@
+def soma (a, b)
+    return a+b
+
+print (soma(1, 4))
+
+
+def subtracao (a, b)
+    return a-b
+
+print (subtracao(5, 3))
+
+def divisao (a, b)
+    return a/b
+
+print (divisao(1, 4))
+
+def multiplicacao (a, b)
+    return a*b
+
+print (multiplicacao(1, 4))
+
+
+
+
