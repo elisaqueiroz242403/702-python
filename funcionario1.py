db983aafe4f449a87505dba981db63337d56f6e1
karen queiroz <kqueiroz812@gmail.com>
2019-08-13 16:33:07 -0300

    adicionar exercicio 1

---
+++ funcionario1.py
@@ -0,0 +1,7 @@
+funcionario = input('Digite seu nome: ')
+horas = float(input('Digite quantas horas você trabalha: '))  
+salario = float(input('Digite quanto você recebe por hora: '))
+
+valor =(horas * salario)
+
+print("Seu salario é: R$" +str(valor))
